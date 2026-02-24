2d19b2ce7cbd70666286ece88a2773a01bb2c5f6
𓆩𝐀𝐘𝐔𝐒𝐇𓆪 ♡ 𝐗ᴅ🪽 <105988266+Ayushjha9988@users.noreply.github.com>
2026-02-24 07:10:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
-import os
 from pyrogram import Client, filters
+import os
 
-API_ID = int(os.environ["API_ID"])
-API_HASH = os.environ["API_HASH"]
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+# Railway Environment Variable se BOT_TOKEN lega
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+# Simple Bot Client (API_ID / API_HASH ki zarurat nahi)
 app = Client(
-    "bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    in_memory=True
+    "mybot",
+    bot_token=BOT_TOKEN
 )
 
+# /start command
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("Bot is alive 🚀")
+    await message.reply_text(
+        "🔥 All In One Bot Running Successfully 🚀"
+    )
 
+# /ping command
+@app.on_message(filters.command("ping"))
+async def ping(client, message):
+    await message.reply_text("🏓 Pong!")
+
+# Bot start
 app.run()
