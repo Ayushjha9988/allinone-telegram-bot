fe2305e1cf6679a3d7b71b0331b69bb4f850cf0e
𓆩𝐀𝐘𝐔𝐒𝐇𓆪 ♡ 𝐗ᴅ🪽 <105988266+Ayushjha9988@users.noreply.github.com>
2026-02-24 07:38:34 +0530

    Refactor bot.py for improved readability and updates

--- bot.py
+++ bot.py
@@ -1,26 +1,22 @@
-from pyrogram import Client, filters
 import os
+from pyrogram import Client
+from pyrogram import filters
 
-# Railway Environment Variable se BOT_TOKEN lega
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-# Simple Bot Client (API_ID / API_HASH ki zarurat nahi)
 app = Client(
-    "mybot",
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
-# /start command
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text(
-        "🔥 All In One Bot Running Successfully 🚀"
-    )
+    await message.reply_text("Bot is working successfully 🚀")
 
-# /ping command
-@app.on_message(filters.command("ping"))
-async def ping(client, message):
-    await message.reply_text("🏓 Pong!")
+print("Bot Started...")
 
-# Bot start
 app.run()
